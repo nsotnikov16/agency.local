ded819ba9f654d72a1e31250645bd6349cbd1c90
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-03-02 00:13:21 +0500

    update

--- dev/template/head.php
+++ dev/template/head.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="ru" class="<?= defined('BG_2') && BG_2 ? 'bg-2' : ''?>">
+<html lang="ru" class="<?= defined('BG_2') && BG_2 ? 'bg-2' : '' ?>">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <title>Агенство</title>
     <link rel="stylesheet" href="<?= ASSETS_PATH ?>css/style.css">
+    <link rel="shortcut icon" href="<?= ASSETS_PATH ?>images/favicon.ico" type="image/x-icon">
 </head>
 
-<body >
+<body>
--- dev/template/views/services-index.php
+++ dev/template/views/services-index.php
@@ -4,22 +4,28 @@
             <h2 class="section__title" data-aos="fade-right"><a href="/services/">Наши услуги</a></h2>
             <ul class="services__list">
                 <li class="service" data-aos="flip-left" data-aos-duration="1200">
-                    <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/razrabotka.svg" alt="Разработка сайтов с нуля">
-                    <div class="service__info">
-                        <p class="service__name">Разработка сайтов с нуля (от лендинга до интернет-магазина)</p>
-                    </div>
+                    <a href="/services/#dev" class="not-effect">
+                        <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/razrabotka.svg" alt="Разработка сайтов с нуля">
+                        <div class="service__info">
+                            <p class="service__name">Разработка сайтов с нуля (от лендинга до интернет-магазина)</p>
+                        </div>
+                    </a>
                 </li>
                 <li class="service" data-aos="flip-right" data-aos-duration="1200">
-                    <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/crm.svg" alt="Индивидуальная разработка CRM для Вашего бизнеса">
-                    <div class="service__info">
-                        <p class="service__name">Индивидуальная разработка CRM для Вашего бизнеса</p>
-                    </div>
+                    <a href="/services/#crm" class="not-effect">
+                        <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/crm.svg" alt="Индивидуальная разработка CRM для Вашего бизнеса">
+                        <div class="service__info">
+                            <p class="service__name">Индивидуальная разработка CRM для Вашего бизнеса</p>
+                        </div>
+                    </a>
                 </li>
                 <li class="service" data-aos="flip-left" data-aos-duration="1200">
-                    <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/support.svg" alt="Техническая поддержка">
-                    <div class="service__info">
-                        <p class="service__name">Техническая поддержка</p>
-                    </div>
+                    <a href="/services/#support" class="not-effect">
+                        <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/support.svg" alt="Техническая поддержка">
+                        <div class="service__info">
+                            <p class="service__name">Техническая поддержка и модернизация</p>
+                        </div>
+                    </a>
                 </li>
                 <!-- <li class="service" data-aos="flip-right" data-aos-duration="1200">
                     <img class="service__img" data-lazy-src="<?= ASSETS_PATH ?>images/services/seo.svg" alt="SEO продвижение">
