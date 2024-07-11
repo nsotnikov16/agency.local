9fd68ca9e9a8f5f00e7e0633701f7d0e1efa36c5
Nikita Sotnikov <nsotnikov16@yandex.ru>
2024-07-12 00:48:52 +0500

    update

--- dev/template/views/projects-index.php
+++ dev/template/views/projects-index.php
@@ -2,12 +2,20 @@
     <div class="container">
         <h2 class="section__title" data-aos="fade-right">Выполненные проекты</h2>
         <ul class="projects__list">
+            <li class="project" data-aos="zoom-in" data-aos-duration="2000">
+                <a href="https://exchange365.ru/" target="_blank" class="project__wrapper not-effect">
+                    <div class="project__feat">
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/exchange.png" alt="Exchange365">
+                    </div>
+                    <div class="project__info">
+                        <span class="project__title link">EXchange 365</span> - сервис электронных журналов
+                    </div>
+                </a>
+            </li>
             <li class="project project_easygoenglish" data-aos="zoom-in" data-aos-duration="2000">
                 <a href="https://easygoenglish.ru/" target="_blank" class="project__wrapper not-effect">
                     <div class="project__feat">
-                        <!-- <video class="project__video" playsinline autoplay muted loop data-lazy-src="<?= ASSETS_PATH ?>video/easygoenglish.mp4"></video> -->
-                        <img class="project__img" loading="lazy"
-                             src="<?= ASSETS_PATH ?>images/projects/easygoenglish.png" alt="EasyGoEnglish">
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/easygoenglish.png" alt="EasyGoEnglish">
                     </div>
                     <div class="project__info">
                         <span class="project__title link">EasyGoEnglish</span> - курсы английского языка
@@ -18,22 +26,18 @@
                 <a href="https://sochi.niko-m.ru/" target="_blank" class="project__wrapper not-effect">
 
                     <div class="project__feat">
-                        <video class="project__video" playsinline autoplay muted loop
-                               data-lazy-src="<?= ASSETS_PATH ?>video/niko.mp4"></video>
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/niko.png"
-                             alt="Niko-Media Landing">
+                        <video class="project__video" playsinline autoplay muted loop data-lazy-src="<?= ASSETS_PATH ?>video/niko.mp4"></video>
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/niko.png" alt="Niko-Media Landing">
                     </div>
                     <div class="project__info">
                         <span class="project__title link">Niko-Media</span> - наружная реклама
                     </div>
                 </a>
             </li>
-            <li class="project m-auto" data-aos="zoom-in" data-aos-duration="2000">
-                <a href="https://neiros.ru/help/rabota-s-konstruktorom-chat-botov/" target="_blank"
-                   class="project__wrapper not-effect">
-                    <div class="project__feat" style="transform: translate3d(0px, -6px, 0) rotate(-1deg);">
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/neiros.png"
-                             alt="Neiros">
+            <li class="project project_neiros" data-aos="zoom-in" data-aos-duration="2000">
+                <a href="https://neiros.ru/help/rabota-s-konstruktorom-chat-botov/" target="_blank" class="project__wrapper not-effect">
+                    <div class="project__feat" >
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/neiros.png" alt="Neiros">
                     </div>
                     <div class="project__info">
                         <span class="project__title link">Neiros</span> - конструктор чат-ботов
--- projects/index.php
+++ projects/index.php
@@ -9,16 +9,26 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/dev/tools/core.php'; ?>
     <div class="container">
         <h1 class="hero__title text-center" data-aos="fade-down" data-aos-duration="1000" data-aos-delay="0">Наши выполненные проекты</h1>
         <span class="hero__img" style="max-width: 400px;" data-img-svg data-src="<?= ASSETS_PATH
-        ?>images/about/group-presentation.svg" ></span>
+                                                                                    ?>images/about/group-presentation.svg"></span>
     </div>
 </section>
 <section class="section projects">
     <div class="container">
         <ul class="projects__list">
+            <li class="project" data-aos="zoom-in" data-aos-duration="2000">
+                <a href="https://exchange365.ru/" target="_blank" class="project__wrapper not-effect">
+                    <div class="project__feat">
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/exchange.png" alt="Exchange365">
+                    </div>
+                    <div class="project__info">
+                        <span class="project__title link">EXchange 365</span> - сервис электронных журналов
+                    </div>
+                </a>
+            </li>
             <li class="project project_easygoenglish" data-aos="zoom-in" data-aos-duration="2000">
                 <a href="https://easygoenglish.ru/" target="_blank" class="project__wrapper not-effect">
                     <div class="project__feat">
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/easygoenglish.png"  alt="EasyGoEnglish"/>
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/easygoenglish.png" alt="EasyGoEnglish" />
                     </div>
                     <div class="project__info">
                         <span class="project__title link">EasyGoEnglish</span> - курсы английского языка
@@ -29,8 +39,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/dev/tools/core.php'; ?>
                 <a href="https://sochi.niko-m.ru/" target="_blank" class="project__wrapper not-effect">
                     <div class="project__feat">
                         <video class="project__video" playsinline autoplay muted loop data-lazy-src="<?= ASSETS_PATH ?>video/niko.mp4"></video>
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/niko.png"
-                             alt="Niko-Media Landing" />
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/niko.png" alt="Niko-Media Landing" />
                     </div>
                     <div class="project__info">
                         <span class="project__title link">Niko-Media</span> - наружная реклама
@@ -40,8 +49,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/dev/tools/core.php'; ?>
             <li class="project m-auto" data-aos="zoom-in" data-aos-duration="2000">
                 <a href="https://neiros.ru/help/rabota-s-konstruktorom-chat-botov/" target="_blank" class="project__wrapper not-effect">
                     <div class="project__feat" style="transform: translate3d(0px, -6px, 0) rotate(-1deg);">
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/neiros.png"
-                             alt="Neiros"/>
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/neiros.png" alt="Neiros" />
                     </div>
                     <div class="project__info">
                         <span class="project__title link">Neiros</span> - конструктор чат-ботов
@@ -51,8 +59,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/dev/tools/core.php'; ?>
             <li class="project" data-aos="zoom-in" data-aos-duration="2000">
                 <a href="https://sauna-pautina.ru/" target="_blank" class="project__wrapper not-effect">
                     <div class="project__feat">
-                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/sauna.png"
-                             alt="Pautina"/>
+                        <img class="project__img" loading="lazy" src="<?= ASSETS_PATH ?>images/projects/sauna.png" alt="Pautina" />
                     </div>
                     <div class="project__info">
                         <span class="project__title link">Паутина</span> - сауна, хамам, СПА
